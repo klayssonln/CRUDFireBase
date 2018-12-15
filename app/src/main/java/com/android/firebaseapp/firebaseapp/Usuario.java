13861e934e50dd10860f92857b66fd97d9c334e4
Klaysson Leite <klayssonln@gmail.com>
2018-12-15 15:15:13 +1300

    Uploading initial files.

---
+++ app/src/main/java/com/android/firebaseapp/firebaseapp/Usuario.java
@@ -0,0 +1,43 @@
+package com.android.firebaseapp.firebaseapp;
+
+public class Usuario {
+    private String nome;
+    private String sobrenome;
+    private String Sexo;
+    private int idade;
+
+    public Usuario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String sexo) {
+        Sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
